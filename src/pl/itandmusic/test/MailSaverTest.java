e3249de31452c2f090cdbe591f1a9102443aaa3d
damian <damian@damian-Lenovo-G780>
2018-06-22 18:03:45 +0200

    Tests

---
+++ src/pl/itandmusic/test/MailSaverTest.java
@@ -0,0 +1,47 @@
+package pl.itandmusic.test;
+
+import junit.framework.TestCase;
+import pl.itandmusic.mailsaver.MailSaver;
+import pl.itandmusic.mailsender.status.SavingInFolderStatus;
+import pl.itandmusic.model.MailSaverProperties;
+import pl.itandmusic.model.MailSenderProperties;
+
+public class MailSaverTest extends TestCase {
+
+	private MailSenderProperties mailSenderProperties;
+	private MailSaverProperties mailSaverProperties;
+	private MailSaver mailSaver;
+
+	public void setUp() {
+		MailSenderProperties.Builder mailSenderPropertiesBuilder = new MailSenderProperties.Builder();
+
+		mailSenderProperties = mailSenderPropertiesBuilder
+				.setUsername("damianradowieckitest@gmail.com")
+				.setPassword("damianradowieckitest123")
+				.setFrom("damianradowieckitest@gmail.com")
+				.setTo("damianradowieckitest@gmail.com")
+				.setSubject("test")
+				.setText("test")
+				.setSMTPAuth("true")
+				.setSMTPHost("smtp.gmail.com")
+				.setSMTPPort("587")
+				.setSMTPStartTLSEnable("true")
+				.build();
+
+		mailSaver = new MailSaver();
+
+		MailSaverProperties.QuickBuilder mailSaverPropertiesQuickBuilder = new MailSaverProperties.QuickBuilder();
+
+		mailSaverProperties = mailSaverPropertiesQuickBuilder
+				.setFolderName("[Gmail]/Sent Mail")
+				.setMailSenerProperties(mailSenderProperties)
+				.setStoreProtocol("imaps")
+				.setStoreHost("imap.gmail.com")
+				.build();
+	}
+	
+	public void testMailSaving() {
+		SavingInFolderStatus savingInFolderStatus = mailSaver.tryToSaveInFolder(mailSaverProperties);
+		assertTrue(savingInFolderStatus.equals(SavingInFolderStatus.SAVED));
+	}
+}
---
+++ src/pl/itandmusic/test/MailSenderTest.java
@@ -0,0 +1,38 @@
+package pl.itandmusic.test;
+
+import junit.framework.TestCase;
+import pl.itandmusic.mailsender.MailSender;
+import pl.itandmusic.mailsender.status.SendingStatus;
+import pl.itandmusic.model.MailSenderProperties;
+
+public class MailSenderTest extends TestCase{
+	
+	private MailSenderProperties mailSenderProperties;
+	private MailSender mailSender;
+	
+	public void setUp() {
+
+		MailSenderProperties.Builder mailSenderPropertiesBuilder = new MailSenderProperties.Builder();
+		mailSenderProperties =
+				mailSenderPropertiesBuilder
+				.setUsername("damianradowieckitest@gmail.com")
+				.setPassword("damianradowieckitest123")
+				.setFrom("damianradowieckitest@gmail.com")
+				.setTo("damianradowieckitest@gmail.com")
+				.setSubject("test")
+				.setText("test")
+				.setSMTPAuth("true")
+				.setSMTPHost("smtp.gmail.com")
+				.setSMTPPort("587")
+				.setSMTPStartTLSEnable("true")
+				.build();
+		
+		mailSender = new MailSender();
+	}
+	
+	public void testMailSending() {
+		SendingStatus status = mailSender.tryToSend(mailSenderProperties);
+		assertTrue(status.equals(SendingStatus.SENT));
+	}
+	
+}
